d6fc4cd4afa0181dffaf5d1c7587ec7c0ef9d223
MOUSAM-bit <mousamjha2006@gmail.com>
2026-07-11 15:22:49 +0530

    Optimize memory usage

--- rag/chain.py
+++ rag/chain.py
@@ -1,9 +1,13 @@
 from rag.retriever import get_retriever
 
-retriever = get_retriever()
+retriever = None
 
 
 def retrieve_context(question):
+    global retriever
+
+    if retriever is None:
+        retriever = get_retriever()
 
     docs = retriever.invoke(question)
 
--- rag/retriever.py
+++ rag/retriever.py
@@ -1,11 +1,23 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
+_embeddings = None
+
+
+def get_embeddings():
+    global _embeddings
+
+    if _embeddings is None:
+        _embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    return _embeddings
+
+
 def get_retriever():
 
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
+    embeddings = get_embeddings()
 
     vector_db = FAISS.load_local(
         "vector_store",
@@ -13,8 +25,6 @@ def get_retriever():
         allow_dangerous_deserialization=True
     )
 
-    retriever = vector_db.as_retriever(
+    return vector_db.as_retriever(
         search_kwargs={"k": 3}
     )
-
-    return retriever
