53f585c13a86bd127af2fb2c687af1a70564db96
MOUSAM-bit <mousamjha2006@gmail.com>
2026-07-11 16:35:47 +0530

    Fix Railway port

--- app.py
+++ app.py
@@ -535,7 +535,13 @@ say no suitable college found.
 # ==========================
 
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
+    
